4e655873efa5b4dfa21ee79486a03b2c276360d6
jack lipson <jackwlipson@gmail.com>
2025-08-14 18:30:00 -0700

    changing folder name to gh-pages

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,11 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets"); // Copies src/img to _site/img
     eleventyConfig.addPassthroughCopy("styles"); // Copies src/img to _site/img
 
+    return {
+        dir: {
+            output: "gh-pages"
+        }
+    }
 
     // return {
     // dir: {
